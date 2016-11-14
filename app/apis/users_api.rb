2244deda5a27230c8e741684768a48cf7da2dcf7
Stephane Bounmy <stephanebounmy@gmail.com>
2016-11-14 09:25:36 +0100

    better fix for user creation

--- app/apis/users_api.rb
+++ app/apis/users_api.rb
@@ -10,30 +10,32 @@ class UsersAPI < Grape::API
       requires :moving_on,              type: Date,   desc: 'Moving date'
     end
 
-    # refactor this ! this api call should not be idempotent
     post do
       house = House.find_by(stripe_publishable_key: declared_params[:stripe_publishable_key])
-      user = house.users.where(email: declared_params[:email]).first ||  house.users.build(declared_params.except(:stripe_publishable_key))
-      user.tap do |u|
+      house.users.build(declared_params.except(:stripe_publishable_key)).tap do |u|
         Stripe.api_key = house.stripe_access_token
 
         c = Stripe::Customer.create(
           source: declared_params[:token], # obtained from Stripe.js
           email:  declared_params[:email]
-        ) if u.stripe_id.nil?
-
-        u.plan = 'basic_monthly' #force by default
-        u.stripe_id ||= c.id
-        u.save!
+        )
 
         # directly subscribes user
-        # this should move to a separate api call
+        begin
         Stripe::Subscription.create(
-          customer: u.stripe_id,
+          customer: c.id,
           plan: "basic_monthly",
           application_fee_percent: 20,
           trial_end: declared_params[:moving_on].to_time.to_i
         )
+        rescue Exception => e
+          c.delete #rollbacks customer creation if any issue and raise
+         raise
+        end
+
+        u.plan = 'basic_monthly' #force by default
+        u.stripe_id = c.id
+        u.save!
       end
     end
 
--- spec/apis/users_api_spec.rb
+++ spec/apis/users_api_spec.rb
@@ -56,7 +56,7 @@ describe UsersAPI do
           expect {
             create_user
           }.to raise_error(Stripe::InvalidRequestError, 'No such plan: basic_monthly')
-        }.to change { User.count }
+        }.to_not change { User.count }
       end
 
       it 'raises error when no matching publishable key' do
@@ -73,12 +73,13 @@ describe UsersAPI do
           expect {
             create_user moving_on: '1988-10-20'
           }.to raise_error(Stripe::InvalidRequestError, 'Invalid timestamp: must be an integer Unix timestamp in the future')
-        }.to change { User.count }
+        }.to_not change { User.count }
       end
 
       it 'does not create duplicate users' do
+        pending
         create_user
-        expect(Stripe::Customer).to_not receive(:create)
+        expect_any_instance_of(Stripe::Customer).to receive(:delete)
         create_user
       end
 
