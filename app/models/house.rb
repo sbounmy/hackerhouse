fac39df831fc9b1dec41b8757be676ba07dc6d78
Stephane Bounmy <stephanebounmy@gmail.com>
2016-11-18 17:54:18 +0100

    fix production and chelou

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,16 +1,36 @@
+# 1 house = 1 stripe account
 class House
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   # HackerHouse name
   field :name, type: String
 
-  # Stripe access token
+  # Description text
+  field :description, type: String
+
+  # Stripe tokens
   field :stripe_access_token, type: String
+  field :stripe_refresh_token, type: String
+  field :stripe_publishable_key, type: String
 
-  # Stripe account id
-  field :stripe_acc_id, type: String
+  # Stripe attributes
+  # - stripe acc_id
+  field :stripe_id, type: String
 
-  # Slack channel id without #
   # it is an unique id
-  field :slack_channel, type: String
+  # Must match a slack channel ID without #
+  field :slug_id, type: String
+
+  # Associations
+  has_many :users
+
+  # Validations
+  validates :slug_id, uniqueness: true, presence: true
+  validates :stripe_id, uniqueness: true, presence: true
+  validates :stripe_access_token, presence: true
+
+  def slack_id
+    "##{slug_id}"
+  end
 end
