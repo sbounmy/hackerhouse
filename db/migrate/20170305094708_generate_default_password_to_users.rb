206ca7e1b666167afb88db428e427ffc8c33c3bd
Stephane Bounmy <stephanebounmy@gmail.com>
2017-03-14 17:16:12 +0100

    saving user in migration

--- db/migrate/20170305094708_generate_default_password_to_users.rb
+++ db/migrate/20170305094708_generate_default_password_to_users.rb
@@ -5,6 +5,7 @@ class GenerateDefaultPasswordToUsers < Mongoid::Migration
         # generate default password atm : stephane@hackerhouse.paris -> stephane42
         user.password = "#{user.email.split('@')[0]}42"
       end
+      user.save!
     end
   end
 
