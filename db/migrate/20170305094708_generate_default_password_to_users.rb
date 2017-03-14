410edbd127e54bd85d9a50e95aa5cae2a0f03e12
Stephane Bounmy <stephanebounmy@gmail.com>
2017-03-14 17:09:12 +0100

    fix when nil

--- db/migrate/20170305094708_generate_default_password_to_users.rb
+++ db/migrate/20170305094708_generate_default_password_to_users.rb
@@ -1,7 +1,7 @@
 class GenerateDefaultPasswordToUsers < Mongoid::Migration
   def self.up
     User.all.each do |user|
-      if user.password_digest.empty?
+      if user.password_digest.nil?
         # generate default password atm : stephane@hackerhouse.paris -> stephane42
         user.password = "#{user.email.split('@')[0]}42"
       end
