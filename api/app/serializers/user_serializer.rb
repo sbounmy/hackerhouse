5b2861c51d43454a88f489bffe8c4d410af65ed7
Stephane Bounmy <stephanebounmy@gmail.com>
2017-04-02 00:27:54 +0200

    expose bio_title and bio_url to api

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes  :id, :firstname, :lastname, :avatar_url
+  attributes  :id, :firstname, :lastname, :avatar_url, :bio_title, :bio_url
 end
