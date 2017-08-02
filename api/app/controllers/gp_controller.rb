57cbf3b7ca27b6548e5475b8f994ffdaa3f4ed11
Stephane Bounmy <stephanebounmy@gmail.com>
2017-08-02 16:50:57 +0200

    allowing iframe

--- api/app/controllers/gp_controller.rb
+++ api/app/controllers/gp_controller.rb
@@ -2,4 +2,12 @@ class GpController < ApplicationController
   def show
     @house = House.find_by(slug_id: params[:slug_id])
   end
+
+  after_action :allow_iframe
+
+  private
+
+  def allow_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://hackerhouse.paris'
+  end
 end
