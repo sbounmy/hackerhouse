633c0dce6244244a91e1e7db28e81a3d77081ab4
Stephane Bounmy <stephanebounmy@gmail.com>
2017-09-13 11:46:48 +0200

    oauth: fix url as config

--- api/lib/app.rb
+++ api/lib/app.rb
@@ -1,8 +1,13 @@
 class App
   API_CONFIG = Rails.application.config_for(:api)
+  OAUTH_CONFIG = Rails.application.config_for(:oauth)
   
   def self.api_url
     API_CONFIG['url']
+  end
+
+  def self.oauth_url(name)
+    OAUTH_CONFIG[name.to_s]['redirect_url']
   end
 
   def self.stripe
