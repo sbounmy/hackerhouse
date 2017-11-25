b2793daa7231d66608bb125d0b0945dee2b39bc9
Stephane Bounmy <stephanebounmy@gmail.com>
2017-11-25 15:48:35 +0100

    api: implemented #days_total method on HouseRent

--- api/app/models/house_rent.rb
+++ api/app/models/house_rent.rb
@@ -1,14 +1,17 @@
 class HouseRent
 
   def initialize(house, date)
-    
+    @house = house
+    @date = date
   end
 
 
   def users
-    
   end
 
+  def days_total
+    @date.end_of_month.day
+  end
   
   private
 
--- api/spec/models/house_rent_spec.rb
+++ api/spec/models/house_rent_spec.rb
@@ -4,11 +4,19 @@ RSpec.describe HouseRent, type: :model do
   let(:hq) { create(:house) }
   let(:stripe) { StripeMock.create_test_helper }
 
+  describe '#days_total' do
+    it 'returns 28 on February' do
+      expect(HouseRent.new(hq, Date.new(2017, 02, 01)).days_total).to eq 28
+    end
+
+    it 'returns 31 on January' do
+      expect(HouseRent.new(hq, Date.new(2017, 01, 01)).days_total).to eq 31
+    end
+  end
+
   describe '' do
 
     it 'returns an array' do
-      HouseRent.new(house, Date.today.month)[user]
-
       House.v2.each do |house|
         HouseRent.new(house, Date.today.month).users.each do |user, amount|
         # house.users_need_to_pay(Date.today).each do |user, amount|
@@ -22,12 +30,13 @@ RSpec.describe HouseRent, type: :model do
   end
 
   describe '#users' do
-    let(:rent) { HouseRent.new(hq, Date.today.month) }
+    let(:rent) { HouseRent.new(hq, Date.today) }
     
     it 'returns an array of users who needs to pay' do
-      rent.users.to be_instance_of(Array)
+      expect(rent.users).to be_instance_of(Array)
     end
 
+    it 'returns as parameter user who have to pay'
     it 'is empty if no one have to pay'
     it 'is idempotent'
   end
