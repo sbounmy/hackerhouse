b264dff356b39649d0cbc0a8b7679300bbca3a2b
Nadia A Calbo <nadia@hackerhouse.paris>
2017-12-03 18:14:29 +0100

    cut the general method to enable externarls calls

--- api/app/models/balance.rb
+++ api/app/models/balance.rb
@@ -8,7 +8,7 @@ include ActiveModel::Serializers::JSON
   end
 
   def users
-    generate_calendar
+    balance_amount
   end
 
   def days_total
@@ -26,38 +26,41 @@ include ActiveModel::Serializers::JSON
   end
 
   def generate_calendar
-    calendar = Array.new(find_users.count) { Array.new(days_total + 1) {1} }
+    @calendar = Array.new(find_users.count) { Array.new(days_total + 1) {1} }
     find_users.each_with_index do |user, i|
-      calendar[i][0] = user
+      @calendar[i][0] = user
 
       #someone leave during the month
       if not (user.check_in..user.check_out).cover? @date.end_of_month
-        calendar[i].fill(0, user.check_out.day)
+        @calendar[i].fill(0, user.check_out.day)
       end
       #someone arrive during the month
       if user.check_in.month === @date.month
-        calendar[i].fill(0, 1, user.check_in.day - 1)
+        @calendar[i].fill(0, 1, user.check_in.day - 1)
       end
-
     end
+    @calendar
+  end
 
-    nb_per_day = Array.new(days_total + 1) {0}.each_with_index.map do |day, i|
-      calendar.inject(0) do |sum, hacker|
+   def nb_per_day 
+      @nb_per_day = Array.new(days_total + 1) {0}.each_with_index.map do |day, i|
+      @calendar.inject(0) do |sum, hacker|
         sum + hacker[i]
       end if i > 0
+      @nb_per_day
     end
 
-    calendar.map do |row|
-      j = 1
-      amount = row[1..-1].inject(0) do |sum, presence|
-        res = 0
-        if not presence.zero?
-          res = ((@house.amount / (nb_per_day[j] * days_total)) - amount_per_day)
+    def balance_amount
+      @calendar.map do |row|
+        j = 1
+        amount = row[1..-1].inject(0) do |sum, presence|
+          res = 0
+          if not presence.zero?
+            res = ((@house.amount / (@nb_per_day[j] * days_total)) - amount_per_day)
+          end
+          j += 1
+          sum + res
         end
-        j += 1
-        sum + res
-      end
       [row[0], amount.ceil]
     end
-  end
 end
