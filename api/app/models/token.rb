d8ce1aa69b122f780c6b5932f6c8fb20ba654f9c
Stephane Bounmy <stephanebounmy@gmail.com>
2018-06-17 19:12:31 +0900

    api: fix linkedin creation when missing publicprofileurl

--- api/app/models/token.rb
+++ api/app/models/token.rb
@@ -11,6 +11,7 @@ class Token
   #  "pictureUrls"=>{"_total"=>1, "values"=>["https://media.licdn.com/mpr/mprx/0_fvQ1tgoS9LfejF3AaLojtpQ7NQlERNSAac4YNpn_0-rdpv0xu5F099S5RF-"]},
   #  "positions"=>{"_total"=>2, "values"=>[{"company"=>{"id"=>10331406, "industry"=>"Computer Software", "name"=>"HackerHouse Paris", "size"=>"2-10", "type"=>"Privately Held"}, "id"=>743434256, "isCurrent"=>true, "location"=>{"country"=>{"code"=>"fr", "name"=>"France"}, "name"=>"Paris Area, France"}, "startDate"=>{"month"=>11, "year"=>2015}, "summary"=>"\"Do not delegate Growth\"\nProduct Market Fit / Growth Hacking", "title"=>"Co-Founder, Growth Lead"}, {"company"=>{"name"=>"Growth Hacking "}, "id"=>902040060, "isCurrent"=>true, "location"=>{"country"=>{"code"=>"fr", "name"=>"France"}, "name"=>"Paris Area, France"}, "startDate"=>{"month"=>1, "year"=>2016}, "summary"=>"Men lie, Women lie but Data don't. Let your metrics speak for yourself 🍄\nGiving 10+ monthly meetups/workshops about growth and hustling. 🚀\nHelped and advised 100+ early stage startups.", "title"=>"Very Active Speaker"}]},
   #  "publicProfileUrl"=>"https://www.linkedin.com/in/stephanebounmy"}
+  #  => https://developer.linkedin.com/docs/ref/v2/profile/basic-profile
   def self.from_linkedin(token, response={})
     data = {
       email: response['emailAddress'],
@@ -19,7 +20,7 @@ class Token
       location: response['location']['name'],
       avatar_url: response['pictureUrl'], # response['pictureUrls']['values'][0] not working
       bio_title: response['headline'],
-      bio_url: response['publicProfileUrl']
+      bio_url: response['publicProfileUrl'] || response['siteStandardProfileRequest']['url'] # return authenticated profile url when not public profile
     }
     new(token, data)
   end
